4ec0e70901db258cb771ffc36c4893357260aef1
hjuanma <jm1995217@hotmail.com>
2023-10-26 21:37:36 -0500

    movies mapper implemented

--- lib/infrastructure/datasources/moviesdb_datasource.dart
+++ lib/infrastructure/datasources/moviesdb_datasource.dart
@@ -2,6 +2,8 @@ import 'package:dio/dio.dart';
 import 'package:grupoalianza_test_cine/config/constants/environment.dart';
 import 'package:grupoalianza_test_cine/config/constants/themoviedb.dart';
 import 'package:grupoalianza_test_cine/domain/entities/movies/movie.dart';
+import 'package:grupoalianza_test_cine/infrastructure/mappers/movie_mappers.dart';
+import 'package:grupoalianza_test_cine/infrastructure/models/moviedb_response.dart';
 
 import '../../domain/datasources/movies_datasource.dart';
 
@@ -15,7 +17,12 @@ class MoviedbDatasource extends MoviesDatasource {
   Future<List<Movie>> getNowPlaging({int page = 1}) async {
     final response =
         await dio.get("${TheMoviesDB.movie}${TheMoviesDB.nowPlaying}");
-    final List<Movie> movies = [];
+
+    final moviedbResponse = MovieDbResponse.fromJson(response.data);
+    final List<Movie> movies = moviedbResponse.results
+        .where((element) => element.posterPath != "no-poster")
+        .map((e) => MovieMapper.movieDBToEntity(e))
+        .toList();
     return movies;
   }
 }
