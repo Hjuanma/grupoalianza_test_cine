93a98a33bb88eec114724013c68d6bc7a85146fa
hjuanma <jm1995217@hotmail.com>
2023-10-26 21:01:48 -0500

    the moviedb constants added

---
+++ lib/config/constants/TheMoviesDB.dart
@@ -0,0 +1,8 @@
+class TheMoviesDB {
+
+  static const String baseUrl = "https://api.themoviedb.org/3/";
+  static const String movie = "movie/";
+  static const String nowPlaying = "now_playing/";
+  
+  
+}
